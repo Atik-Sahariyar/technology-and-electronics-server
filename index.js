936d06a2a4fb5d1738f4576f119461748a630001
Atik Sahariyar <atiksahariyar88@gmail.com>
2023-10-20 06:58:11 +0600

    update product

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5500;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5700;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // Middlewere
@@ -24,9 +24,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollections = client.db('productsDB').collection('products');
+    
+    app.get('/products', async(req, res) => {
+        const cursor = productCollections.find();
+        const result = await cursor.toArray();
+        res.send(result);
+        console.log(result);
+    })
+     
+    // get indevidual product
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productCollections.findOne(query);
+      res.send(result);
+    })
+
 
     app.post('/products', async(req, res) => {
         const newProduct = req.body;
@@ -36,7 +52,7 @@ async function run() {
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your d eployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,7 +64,6 @@ run().catch(console.dir);
 app.get('/', (req, res ) => {
     res.send('Technology and electronics server is running')
 })
-
 
 app.listen(port, () => {
     console.log(`Technology and electronics server is running on port: ${port}`);
